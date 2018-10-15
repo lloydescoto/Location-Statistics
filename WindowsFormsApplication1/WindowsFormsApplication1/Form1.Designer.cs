c1eafb8db0bab079e1a9e14cd3bf0ddc46208222
Lloyd Escoto <42109272+lloydescoto22@users.noreply.github.com>
2018-10-15 23:17:42 +0800

    Putted Finalize Code

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(123, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(102, 312);
+            this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(120, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Location";
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Location = new System.Drawing.Point(15, 87);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(102, 312);
+            this.panel2.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 7);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Eras Medium ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Name : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Eras Medium ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(12, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Location : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(228, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Statistics";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.Location = new System.Drawing.Point(231, 87);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(102, 291);
+            this.panel3.TabIndex = 15;
+            // 
+            // panel4
+            // 
+            this.panel4.AutoScroll = true;
+            this.panel4.Location = new System.Drawing.Point(339, 87);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(35, 291);
+            this.panel4.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(403, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 18);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Graph";
+            // 
+            // panel5
+            // 
+            this.panel5.AutoScroll = true;
+            this.panel5.ForeColor = System.Drawing.SystemColors.Control;
+            this.panel5.Location = new System.Drawing.Point(406, 35);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(102, 364);
+            this.panel5.TabIndex = 18;
+            // 
+            // panel6
+            // 
+            this.panel6.AutoScroll = true;
+            this.panel6.Location = new System.Drawing.Point(514, 35);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 364);
+            this.panel6.TabIndex = 19;
+            // 
+            // panel7
+            // 
+            this.panel7.AutoScroll = true;
+            this.panel7.Location = new System.Drawing.Point(720, 35);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(46, 364);
+            this.panel7.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(231, 381);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 18);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(352, 381);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 18);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(728, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(22, 18);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(778, 417);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Location Statistics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<string> location = new List<string>();
+        Dictionary<string, int> locationCount = new Dictionary<string, int>();
+        Dictionary<string, double> locationPercent = new Dictionary<string, double>();
+        int totalCount = 0;
+        int labelPan1X = 0;
+        int labelPan1Y = 0;
+        int statLabelPanX = 0;
+        int statLabelPanY = 0;
+        int graphLabelPanX = 0;
+        int graphLabelPanY = 0;
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(location.Contains(textBox1.Text))
+            {
+                int count;
+                string currentKey;
+                for(int x = 0;x < locationCount.Count;x++)
+                {
+                    var item = locationCount.ElementAt(x);
+                    if(textBox1.Text == item.Key)
+                    {
+                        currentKey = item.Key;
+                        count = item.Value;
+                        count++;
+                        locationCount[currentKey] = count;
+                    }
+                }
+                panel3.Controls.Clear();
+                panel4.Controls.Clear();
+                statLabelPanY = 0;
+                foreach (var loc in locationCount)
+                {
+                    Label statLocLabel = new Label();
+                    Label statCountLabel = new Label();
+                    statLocLabel.Text = loc.Key;
+                    statLocLabel.ForeColor = Color.White;
+                    statLocLabel.Location = new Point(statLabelPanX, statLabelPanY);
+                    statCountLabel.Text = loc.Value.ToString();
+                    statCountLabel.ForeColor = Color.White;
+                    statCountLabel.Location = new Point(statLabelPanX, statLabelPanY);
+                    statLabelPanY += 21;
+                    panel3.Controls.Add(statLocLabel);
+                    panel4.Controls.Add(statCountLabel);
+                }
+                totalCount = 0;
+                foreach (var loc in locationCount)
+                {
+                    totalCount += loc.Value;
+                }
+                label8.Text = totalCount.ToString();
+                locationPercent.Clear();
+                for (int x = 0; x < locationCount.Count; x++)
+                {
+                    double percent = 0;
+                    var item = locationCount.ElementAt(x);
+                    percent = item.Value * 100 / totalCount;
+                    locationPercent.Add(item.Key, Math.Round(percent, 2));
+                }
+                panel5.Controls.Clear();
+                panel6.Controls.Clear();
+                panel7.Controls.Clear();
+                graphLabelPanY = 0;
+                foreach (var loc in locationPercent)
+                {
+                    Label graphLocName = new Label();
+                    Label graphLocPercent = new Label();
+                    PictureBox graphPicBox = new PictureBox();
+                    graphLocName.Text = loc.Key;
+                    graphLocName.ForeColor = Color.White;
+                    graphLocName.Location = new Point(graphLabelPanX, graphLabelPanY);
+                    graphLocPercent.Text = loc.Value.ToString("F2");
+                    graphLocPercent.ForeColor = Color.White;
+                    graphLocPercent.Location = new Point(graphLabelPanX, graphLabelPanY);
+                    graphPicBox.Location = new Point(graphLabelPanX, graphLabelPanY);
+                    graphPicBox.Height = 12;
+                    graphPicBox.Width = (int)loc.Value * 2;
+                    graphPicBox.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+                    graphLabelPanY += 21;
+                    panel5.Controls.Add(graphLocName);
+                    panel6.Controls.Add(graphPicBox);
+                    panel7.Controls.Add(graphLocPercent);
+                }
+                Label nameLabel = new Label();
+                nameLabel.Text = textBox2.Text;
+                nameLabel.ForeColor = Color.White;
+                nameLabel.Location = new Point(labelPan1X, labelPan1Y);
+                Label label = new Label();
+                label.Text = textBox1.Text;
+                label.ForeColor = Color.White;
+                label.Location = new Point(labelPan1X, labelPan1Y);
+                labelPan1Y += 20;
+                panel1.Controls.Add(label);
+                panel2.Controls.Add(nameLabel);
+            }
+            else
+            {
+                location.Add(textBox1.Text);
+                locationCount.Add(textBox1.Text, 1);
+                totalCount = 0;
+                panel3.Controls.Clear();
+                panel4.Controls.Clear();
+                statLabelPanY = 0;
+                foreach (var loc in locationCount)
+                {
+                    Label statLocLabel = new Label();
+                    Label statCountLabel = new Label();
+                    statLocLabel.Text = loc.Key;
+                    statLocLabel.ForeColor = Color.White;
+                    statLocLabel.Location = new Point(statLabelPanX, statLabelPanY);
+                    statCountLabel.Text = loc.Value.ToString();
+                    statCountLabel.ForeColor = Color.White;
+                    statCountLabel.Location = new Point(statLabelPanX, statLabelPanY);
+                    statLabelPanY += 21;
+                    panel3.Controls.Add(statLocLabel);
+                    panel4.Controls.Add(statCountLabel);
+                }
+                foreach (var loc in locationCount)
+                {
+                    totalCount += loc.Value;
+                }
+                label8.Text = totalCount.ToString();
+                locationPercent.Clear();
+                for (int x = 0; x < locationCount.Count; x++)
+                {
+                    double percent = 0;
+                    var item = locationCount.ElementAt(x);
+                    percent = item.Value * 100 / totalCount;
+                    locationPercent.Add(item.Key, Math.Round(percent,2));
+                }
+                panel5.Controls.Clear();
+                panel6.Controls.Clear();
+                panel7.Controls.Clear();
+                graphLabelPanY = 0;
+                foreach (var loc in locationPercent)
+                {
+                    Label graphLocName = new Label();
+                    Label graphLocPercent = new Label();
+                    PictureBox graphPicBox = new PictureBox();
+                    graphLocName.Text = loc.Key;
+                    graphLocName.ForeColor = Color.White;
+                    graphLocName.Location = new Point(graphLabelPanX, graphLabelPanY);
+                    graphLocPercent.Text = loc.Value.ToString("F2");
+                    graphLocPercent.ForeColor = Color.White;
+                    graphLocPercent.Location = new Point(graphLabelPanX, graphLabelPanY);
+                    graphPicBox.Location = new Point(graphLabelPanX, graphLabelPanY);
+                    graphPicBox.Height = 12;
+                    graphPicBox.Width = (int)loc.Value * 2;
+                    graphPicBox.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+                    graphLabelPanY += 21;
+                    panel5.Controls.Add(graphLocName);
+                    panel6.Controls.Add(graphPicBox);
+                    panel7.Controls.Add(graphLocPercent);
+                }
+                Label nameLabel = new Label();
+                nameLabel.Text = textBox2.Text;
+                nameLabel.ForeColor = Color.White;
+                nameLabel.Location = new Point(labelPan1X, labelPan1Y);
+                Label label = new Label();
+                label.Text = textBox1.Text;
+                label.ForeColor = Color.White;
+                label.Location = new Point(labelPan1X,labelPan1Y);
+                labelPan1Y += 20;
+                panel1.Controls.Add(label);
+                panel2.Controls.Add(nameLabel);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            location.Clear();
+            locationCount.Clear();
+            locationPercent.Clear();
+            panel1.Controls.Clear();
+            panel2.Controls.Clear();
+            panel3.Controls.Clear();
+            panel4.Controls.Clear();
+            panel5.Controls.Clear();
+            panel6.Controls.Clear();
+            panel7.Controls.Clear();
+            totalCount = 0;
+            labelPan1X = 0;
+            labelPan1Y = 0;
+            statLabelPanX = 0;
+            statLabelPanY = 0;
+            graphLabelPanX = 0;
+            graphLabelPanY = 0;
+            label8.Text = totalCount.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
